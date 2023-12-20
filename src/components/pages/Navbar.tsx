ddb915ac8a87eb71b0be8452b3804cc2fe36f448
Codinglone <codinglone@gmail.com>
2023-12-20 18:51:09 +0200

    Removed unwanted links

--- src/components/pages/Navbar.tsx
+++ src/components/pages/Navbar.tsx
@@ -12,13 +12,13 @@ export default function Navbar () {
         <div onClick={toggleMe} className="md:hidden bg-mobile h-20  md:h-0 w-full  md:w-0" >
                 < FaBars size={45} style={{ color: 'white' }} className='ml-3 pt-3' />
         </div>
-       <div className="h-0 md:h-0 w-full md:w-1/2 relative bg-mobile z-50">
+       <div className="h-0 md:h-0 w-full md:w-[46%] relative bg-mobile z-50">
             <div className="hidden md:block">
                 <div className="flex justify-between h-20 w-full ml-20 md:ml-16">
                 <Link className="pt-10 text-white text-xl font-Poppins hover:font-bold  hover:text-gold" to={FrontEndEnums.Home} >Home</Link>
                 <Link className="pt-10 text-white text-xl font-Poppins hover:font-bold  hover:text-gold" to={FrontEndEnums.About}>About</Link>
                 <Link className="pt-10 text-white  text-xl font-Poppins hover:font-bold  hover:text-gold" to={FrontEndEnums.Service}>Services</Link>
-                <Link className="pt-10  text-white text-xl font-Poppins hover:font-bold  hover:text-gold" to={FrontEndEnums.Support }>Support</Link>
+                {/* <Link className="pt-10  text-white text-xl font-Poppins hover:font-bold  hover:text-gold" to={FrontEndEnums.Support }>Support</Link> */}
                 <Link className="pt-10 text-white font-bold text-xl font-Poppins hover:text-gold" to={FrontEndEnums.JoinCommunity }>Join our Community</Link>
                 </div>
             </div>
@@ -30,7 +30,7 @@ export default function Navbar () {
             </div>
             <div className='h-10 ml-6 w-5/6 md:border-0 border-b-2 border-gray-400'><Link className=" text-white md:text-black text-xl hover:font-bold font-Poppins  hover:text-gold" to={FrontEndEnums.About}>About</Link></div>
             <div className='h-10 ml-6 w-5/6 md:border-0 border-b-2 border-gray-400'><Link className=" text-white md:text-black  text-xl hover:font-bold font-Poppins  hover:text-gold" to={FrontEndEnums.Service}>Services</Link></div>
-            <div className='h-10 ml-6 w-5/6 md:border-0 border-b-2 border-gray-400'><Link className="  text-white md:text-black text-xl hover:font-bold font-Poppins  hover:text-gold" to={FrontEndEnums.Support }>Support</Link></div>
+            {/* <div className='h-10 ml-6 w-5/6 md:border-0 border-b-2 border-gray-400'><Link className="  text-white md:text-black text-xl hover:font-bold font-Poppins  hover:text-gold" to={FrontEndEnums.Support }>Support</Link></div> */}
             <div className='h-10 ml-6 w-5/6 md:border-0 border-b-2 border-gray-400'><Link className=" md:text-black  text-white text-xl hover:font-bold font-Poppins  hover:text-gold" to={FrontEndEnums.JoinCommunity }>Join our Community</Link></div>
 
             </div>
